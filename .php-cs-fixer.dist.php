b08d6330e9ac329b937c75066221312c8ec7b702
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-09-13 15:46:41 -0400

    Add finder to PHP CS fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,17 @@
 <?php
 
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('somedir')
+    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+    ->in(__DIR__)
+;
+
 $config = new PhpCsFixer\Config();
 return $config->setRules([
         '@PSR12' => true,
         '@PhpCsFixer' => true,
         '@PHP81Migration' => true
     ])
+    ->setFinder($finder)
 ;
