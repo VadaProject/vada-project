c6be4c36fcce98574c4e77eb693eb212cea69488
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-09-14 15:42:03 -0400

    Add bootstrap.js to <head>

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -19,3 +19,4 @@ switch ($_SERVER['SCRIPT_NAME']) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="assets/stylesheets/global.css">
 <script src="assets/scripts/jquery-3.3.1.min.js" type="text/javascript"></script>
+<script src="assets/scripts/bootstrap.js" type="text/javascript"></script>
