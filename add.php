59de982a247b0263ab21356b913a71065739f937
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-09-21 16:15:48 -0400

    Refactor add.php

--- add.php
+++ add.php
@@ -1,15 +1,19 @@
 <?php include 'config/db_connect.php'; ?>
 <?php include 'includes/page_top.php'; ?>
 <?php
-$claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST = $ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID = $example = $URL = $rd = $reason = $flagType = $flagType = $flagTypeT = $flagTypeR = $flagTypeE = $flagURL = $flagSource = $flagID = $inferenceIDFlagger = $grammar = $active = '';
+$claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST =
+$ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID =
+$example = $URL = $rd = $reason = $flagType = $flagType = $flagTypeT = $flagTypeR = $flagTypeE =
+$flagURL = $flagSource = $flagID = $inferenceIDFlagger = $grammar = $active = '';
 session_start();
 ?>
 <main class="page-container text-center">
-<script>
+    <script>
     $(document).ready(function() {
         $("#submit").click(function() {
             window.alert("Submitted!");
-            window.location.assign("http://localhost/directory/index.php");
+            window.location.assign(
+                "http://localhost/directory/index.php");
             $.post($("#myForm").attr("action"),
                 $("#myForm :input").serializeArray(),
                 function(info) {
@@ -27,81 +31,83 @@ session_start();
             });
         }
     });
-</script>
-<form method="POST" id="myForm" action="insert.php">
-    <?php
-    // $addPage = 'yes';
-    // $_SESSION['addPage'] = $addPage;
-    // get add page variable from another page, hopefully thru post variable.
-    // vada recruitment proposal
-    // firefox working? not working?
-    // rivals at the leaf level not working? ----> if rival, active = 0
-    // NA
-    // AS IN THE CASE OF : SPACE IN EXAMPLE STATEMENT
-    ?>
-    <?php
-    if (isset($_GET['topic'])) {
-    ?> <label>Topic</label><br>
-        <input type="text" name="topic" value="<?php echo $_GET['topic']; ?>" readonly><br>
-    <?php
-    } else {
-    ?>
+    </script>
+    <form method="POST" id="myForm" action="insert.php">
+        <?php if (isset($_GET['topic'])) { ?>
         <label>Topic</label><br>
-        <input type="text" name="topic" value="<?php echo htmlspecialchars($topic); ?>"><br>
-    <?php }
-    ?>
-    <p style="color:#000000" ;>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label><u>Subject</u> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <u>Target Property </u></label><br>
-        <textarea class="subject" type="text" id="subject" name="subject" value="<?php echo htmlspecialchars($subject); ?>">Enter Subject</textarea>
-        <textarea class="targetP" type="text" id="targetP" name="targetP" value="<?php echo htmlspecialchars($targetP); ?>">Enter Target Property</textarea>
-        <br>
-        <u>
-            <p style="color:grey">Thesis Statement
-        </u><br>
-        <span class="jsValue3">subject</span> <span class="jsValue4">target</span>
-    </p>
-    </p>
-    <p style="color:black"> Is the subject an object or a person?
-        <select name="grammar" id="grammar" value="grammar">
-            <option value="">Choose One</option>
-            <option value="object">Object</option>
-            <option value="person">Person</option>
-        </select> <br>
-        What is your Support Means?
-        <select name="union" id="union" value="union">
-            <option value="">Choose One</option>
-            <option value="Inference">Inference</option>
-            <option value="Testimony">Testimony</option>
-            <option value="Perception">Perception</option>
-            <option value="Tarka">Tarka</option>
-        </select>
-    </p>
-    </p>
-    <div id="hiddenRule">
-        <div id="some-div">
-            <img src="assets/img/question_mark.png">
-            <span id="explain-element"> Hint: Your reason statement should answer "Why?". You might think of the reason as what comes after 'because....'. </span>
-        </div>
-        <p style="color:black"><u>Reason</u><br>
-            <textarea class="reason" type="text" id="reason" name="reason" value="<?php echo htmlspecialchars($reason); ?>">Enter Reason Property</textarea>
+        <input type="text" name="topic" value="<?php echo $_GET['topic']; ?>"
+            readonly><br>
+        <?php } else { ?>
+        <label>Topic</label><br>
+        <input type="text" name="topic"
+            value="<?php echo htmlspecialchars($topic); ?>"><br>
+        <?php } ?>
+        <p style="color:#000000" ;>
+            <label><u>Subject</u>
+                <u>Target Property </u></label><br>
+            <textarea class="subject" type="text" id="subject" name="subject"
+                value="<?php echo htmlspecialchars($subject); ?>">Enter Subject</textarea>
+            <textarea class="targetP" type="text" id="targetP" name="targetP"
+                value="<?php echo htmlspecialchars($targetP); ?>">Enter Target Property</textarea>
+            <br>
+            <u>
+                <p style="color:grey">Thesis Statement
+            </u><br>
+            <span class="jsValue3">subject</span> <span
+                class="jsValue4">target</span>
         </p>
-        <u> Reason Statement </u><br>
-        <span class="jsValue5">subject</span> <span class="jsValue6">reason</span>
-        <br><br>
-        <u> Rule and Example Statement </u><br>
-        Whatever/Whomever
-        <!-- Plain Javascript Example -->
-        <span class="jsValue">reason</span>,
-        <span class="jsValue2">target</span>,
-        as in the case of:
-    </div>
-    <br>
-    <textarea id="example" name="example" value="<?php echo htmlspecialchars($example); ?>">Enter Example</textarea>
-    <textarea id="transcription" name="transcription" value="<?php echo htmlspecialchars($transcription); ?>">Transcription </textarea><br>
-    <textarea id="citation" name="citation" value="<?php echo htmlspecialchars($citation); ?>">Please include: Author, title, publication, and date of publication.  </textarea><br>
-    <textarea id="url" name="url" value="<?php echo htmlspecialchars($url); ?>">Enter URL</textarea><br>
-    <textarea id="vidtimestamp" name="vidtimestamp" value="<?php echo htmlspecialchars($vidtimestamp); ?>">Enter timestamp of specified material</textarea>
-    <script>
+        </p>
+        <p style="color:black"> Is the subject an object or a person?
+            <select name="grammar" id="grammar" value="grammar">
+                <option value="">Choose One</option>
+                <option value="object">Object</option>
+                <option value="person">Person</option>
+            </select> <br>
+            What is your Support Means?
+            <select name="union" id="union" value="union">
+                <option value="">Choose One</option>
+                <option value="Inference">Inference</option>
+                <option value="Testimony">Testimony</option>
+                <option value="Perception">Perception</option>
+                <option value="Tarka">Tarka</option>
+            </select>
+        </p>
+        </p>
+        <div id="hiddenRule">
+            <div id="some-div">
+                <img src="assets/img/question_mark.png">
+                <span id="explain-element"> Hint: Your reason statement should
+                    answer "Why?". You might think of the
+                    reason as what comes after 'because....'. </span>
+            </div>
+            <p style="color:black"><u>Reason</u><br>
+                <textarea class="reason" type="text" id="reason" name="reason"
+                    value="<?php echo htmlspecialchars($reason); ?>">Enter Reason Property</textarea>
+            </p>
+            <u> Reason Statement </u><br>
+            <span class="jsValue5">subject</span> <span
+                class="jsValue6">reason</span>
+            <br><br>
+            <u> Rule and Example Statement </u><br>
+            Whatever/Whomever
+            <!-- Plain Javascript Example -->
+            <span class="jsValue">reason</span>,
+            <span class="jsValue2">target</span>,
+            as in the case of:
+        </div>
+        <br>
+        <textarea id="example" name="example"
+            value="<?php echo htmlspecialchars($example); ?>">Enter Example</textarea>
+        <textarea id="transcription" name="transcription"
+            value="<?php echo htmlspecialchars($transcription); ?>">Transcription </textarea><br>
+        <textarea id="citation" name="citation"
+            value="<?php echo htmlspecialchars($citation); ?>">
+            Please include: Author, title, publication, and date of publication.</textarea><br>
+        <textarea id="url" name="url"
+            value="<?php echo htmlspecialchars($url); ?>">Enter URL</textarea><br>
+        <textarea id="vidtimestamp" name="vidtimestamp"
+            value="<?php echo htmlspecialchars($vidtimestamp); ?>">Enter timestamp of specified material</textarea>
+        <script>
         // This is for reason
         var $jsReason = document.querySelector('.reason');
         var $jsValue = document.querySelector('.jsValue');
@@ -143,9 +149,9 @@ session_start();
         $jsReason2.addEventListener('input', function(event) {
             $jsValue6.innerHTML = $jsReason2.value;
         }, false);
-    </script>
-    </div>
-    <script type="text/javascript">
+        </script>
+        </div>
+        <script type="text/javascript">
         var union = document.getElementById('union');
         union.onchange = checkOtherUnion;
         union.onchange();
@@ -183,19 +189,22 @@ session_start();
                 citation.style.display = '';
             }
             if (union.options[union.selectedIndex].value === 'Tarka') {
-                window.alert("A requirement of Tarka is to use the comments feature in the Tarka claim following submission.");
+                window.alert(
+                    "A requirement of Tarka is to use the comments feature in the Tarka claim following submission."
+                );
                 citation.style.display = 'none';
             }
         }
-    </script>
-    <script type="text/javascript">
+        </script>
+        <script type="text/javascript">
         var union = document.getElementById('union');
         union.onchange = checkOtherUnion;
         union.onchange();
-    </script>
-    <br>
-    <button onclick="setTimeout(myFunction, 8000)" id="submit">Submit</button>
-    <script>
+        </script>
+        <br>
+        <button onclick="setTimeout(myFunction, 8000)"
+            id="submit">Submit</button>
+        <script>
         // BELOW IS WHERE SUBMIT BUTTON DISABLED HAPPENS
         jQuery("#submit").prop('disabled', true);
         var card = document.getElementById("union");
@@ -217,7 +226,8 @@ session_start();
                 } else {
                     validTextArea = false;
                 }
-                if (validTextArea == true && validDropDown == true) {
+                if (validTextArea == true && validDropDown ==
+                    true) {
                     jQuery("#submit").prop('disabled', false);
                 } else {
                     jQuery("#submit").prop('disabled', true);
@@ -229,7 +239,11 @@ session_start();
         var toValidate2 = jQuery('#union, #grammar');
         validDropDown = false;
         toValidate2.change(function() {
-            if (jQuery(this)[0].selectedIndex == 1 || jQuery(this)[0].selectedIndex == 2 || jQuery(this)[0].selectedIndex == 3 || jQuery(this)[0].selectedIndex == 4 || jQuery(this)[0].selectedIndex == 5) {
+            if (jQuery(this)[0].selectedIndex == 1 || jQuery(this)[0]
+                .selectedIndex == 2 || jQuery(this)[0]
+                .selectedIndex == 3 || jQuery(this)[0].selectedIndex ==
+                4 || jQuery(this)[0].selectedIndex == 5
+            ) {
                 jQuery(this).data('valid', true);
             } else {
                 jQuery(this).data('valid', false);
@@ -241,7 +255,8 @@ session_start();
                     validDropDown = false;
                     //           window.alert(jQuery(this)[0].selectedIndex);
                 }
-                if (validTextArea == true && validDropDown == true) {
+                if (validTextArea == true && validDropDown ==
+                    true) {
                     jQuery("#submit").prop('disabled', false);
                 } else {
                     jQuery("#submit").prop('disabled', true);
@@ -255,7 +270,7 @@ session_start();
         });
         // above IS WHERE SUBMIT BUTTON DISABLED HAPPENS
         //need to bypass insert page
-    </script>
-</form>
+        </script>
+    </form>
 </main>
 <?php include 'includes/page_bottom.php'; ?>
