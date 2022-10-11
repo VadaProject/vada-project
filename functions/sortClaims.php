e219ee6328f19d595112754bd6b6a74051e48a5f
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-10-10 23:57:06 -0400

    Deep refactor sortClaims

--- functions/sortClaims.php
+++ functions/sortClaims.php
@@ -1,145 +1,172 @@
 <?php
+
 require_once __DIR__ . '/../config/db_connect.php';
-// starts two chains of recursion. one with normal root claims.
-// the other with root rivals. the rivals, of course, are put into the rival recursion.
 
 /*
 This function displays each individual claim in a recursive manner.
 Each recursion is a series of tracking relationships between the claims (found in the Flabsdb).
 */
 
-function sortclaims($claimid)
+/////////////////////////
+// PREPARED STATEMENTS //
+/////////////////////////
+
+/**
+ * @param int $claimID
+ * @param mysqli $conn the mysqli connection to use
+ * @return array an associative array of strings representing the claim
+ */
+function get_claim($claimID, $conn)
 {
-    $conn = db_connect();
+    $stmt = $conn->prepare('SELECT DISTINCT * from claimsdb where claimID = ?');
+    $stmt->bind_param('i', $claimID);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc();
+}
 
-    // THIS IS SIMPLY FOR DISPLAY OF SUBJECT/TARGETP BELOW
+/**
+ * @param int $claimID
+ * @param mysqli $conn the mysqli connection to use
+ * @return
+ */
+function get_rival_flags($claimID, $conn)
+{
+    $stmt = $conn->prepare(
+        'SELECT DISTINCT * from flagsdb WHERE claimIDFlagger = ?'
+    );
+    $stmt->bind_param('i', $claimID);
+    $stmt->execute();
+    return $stmt->get_result(); // get the mysqli result
+}
 
-    $dis = 'SELECT DISTINCT subject, targetP, active, supportMeans, reason, example, citation
-    from claimsdb
-    where ? = claimID
+// look for normal non-rival flags for this rivaling claim.
+/**
+ * Returns the claims which flag $claimID and are not Thesis rivals.
+ *
+ * @param int $claimID
+ * @param mysqli $conn the mysqli connection to use
+ * @return mysqli_result|false
+ */
+function get_non_flag_rivals($claimID, $conn)
+{
+    $stmt = $conn->prepare("SELECT DISTINCT claimIDFlagger
+        from claimsdb, flagsdb
+        where claimIDFlagged = ?
+        AND flagType NOT LIKE 'Thesis Rival'");
+    $stmt->bind_param('i', $claimID);
+    $stmt->execute();
+    return $stmt->get_result();
+}
 
-        '; // SQL with parameters
-    $st = $conn->prepare($dis);
-    $st->bind_param('i', $claimid);
-    $st->execute();
-    $disp = $st->get_result(); // get the mysqli result
+/**
+ * Returns the claims which flag $claimID and are not Thesis rivals.
+ *
+ * @param int $claimID
+ * @param mysqli $conn the mysqli connection to use
+ * @return mysqli_result|false
+ */
+function get_flagged_rivals($claimID, $conn)
+{
+    $stmt = $conn->prepare("SELECT DISTINCT claimIDFlagger
+        from claimsdb, flagsdb
+        where claimIDFlagged = ?
+        AND flagType LIKE 'Thesis Rival'");
+    $stmt->bind_param('i', $claimID);
+    $stmt->execute();
+    return $stmt->get_result();
+}
 
-    // SIMPLY FOR DISPLAY ABOVE THIS POINT
-
-    // below is for rivals
-    $flag = 'SELECT DISTINCT flagType, claimIDFlagger, claimIDFlagged
-        from flagsdb
-        WHERE ? = claimIDFlagger'; // SQL with parameters
-    $stmt4 = $conn->prepare($flag);
-    $stmt4->bind_param('i', $claimid);
-    $stmt4->execute();
-    $result2 = $stmt4->get_result(); // get the mysqli result
-    $numhitsflag = mysqli_num_rows($result2);
-    // IF THIS CLAIM IS A FLAGGER this obtains the FLAGGER'S flagtype's and flagged.
-    // this is to find rival claims..this is literally JUST used for rivals.
-    // rivals have to be flaggers and flagged.
-
-    $resultFlagType = $r = $d = '';
-    while ($flagge = $result2->fetch_assoc()) {
-        $resultFlagType = $flagge['flagType'];
-        $r = $flagge['claimIDFlagger'];
-
-        $d = $flagge['claimIDFlagged'];
+//////////////////////////////////////////////
+// HTML
+//////////////////////////////////////////////
+function make_label_el($claim_id, $claim, $flag_type)
+{
+    ?>
+    <label for="<?php echo $claim_id; ?>">
+    <?php
+    switch ($flag_type) {
+        case 'supporting':
+            echo "<img height = '45' width = '32' src='assets/img/support.png'> <br>";
+            echo $claim['supportMeans'] . '<br>';
+            if ($claim['supportMeans'] == 'Inference') {
+                echo 'Reason: ' .
+                    $claim['subject'] .
+                    ' ' .
+                    $claim['reason'] .
+                    ', as in the case of ' .
+                    $claim['example'] .
+                    '<BR>';
+            }
+            if (
+                $claim['supportMeans'] == 'Testimony' ||
+                $claim['supportMeans'] == 'Perception'
+            ) {
+                echo 'Citation: ' . $claim['citation'] . '<BR>';
+            }
+            break;
+        case '':
+            echo '<h1>Thesis</h1>';
+            echo '<br>' . $claim['subject'] . ' ' . $claim['targetP'] . '<br>';
+            break;
+        default:
+            echo "<img src='assets/img/flag.png'> <br>";
+            echo '<br> Flagged: ' . $flag_type . '<br>';
+            echo '<h1>Thesis</h1>';
+            echo '<br>' . $claim['subject'] . ' ' . $claim['targetP'] . '<br>';
     }
-    // above is for rivals
-
-    if ('Thesis Rival' == $resultFlagType) {
-        echo ' <br> The flag ' . $r . ' has a rival!: ' . '<br>';
-        // ECHO "START 1";
-        sortclaimsRival($r);
-        // ECHO "END 1";
-        // for THIS claimid - check for flaggers that aren't rival .. sort claim those
-
-        // ECHO "START 2";
-        sortclaimsRival($d);
-        // ECHO "END 2";
-        // for the CORRESPONDING claimid - check for flaggers that aren't rival .. sort claim those.
-    } else {
-         ?>
-        <li> <label for="<?php echo $claimid; ?>"><?php while (
-    $d = $disp->fetch_assoc()
-) {
-    if ('supporting' == $resultFlagType) {
-        echo "<img height = '45' width = '32' src='assets/img/support.png'> <br>";
-        echo $d['supportMeans'] . '<br>';
-        if ('Inference' == $d['supportMeans']) {
-            echo 'Reason: ' .
-                $d['subject'] .
-                ' ' .
-                $d['reason'] .
-                ', as in the case of ' .
-                $d['example'] .
-                '<BR>';
-        }
-
-        if (
-            'Testimony' == $d['supportMeans'] ||
-            'Perception' == $d['supportMeans']
-        ) {
-            echo 'Citation: ' . $d['citation'] . '<BR>';
-        }
-    } elseif ('' == $resultFlagType) {
-        echo '<h1>Thesis</h1>';
-        echo '<br>' . $d['subject'] . ' ' . $d['targetP'] . '<br>';
-    } else {
-        echo "<img src='assets/img/flag.png'> <br>";
-
-        echo '<br> Flagged: ' . $resultFlagType . '<br>';
-        echo '<h1>Thesis</h1>';
-        echo '<br>' . $d['subject'] . ' ' . $d['targetP'] . '<br>';
-    }
-    echo '#' . $claimid . '<br>';
+    echo '#' . $claim_id . '<br>';
 
     // add is subject person or object to inference div
 
     // FONT CHANGING
-    if (1 == $d['active']) {
-        // $font = 'seagreen';
-        // echo "<img width='50' height='50' src='assets/img/check_mark.php'> <br>! This claim is uncontested. <br>";
-    } else {
-        // $font = '#B7B802';
+    if ($claim['active'] != 1) {
         echo "<img src='assets/img/alert.png'> <br>";
     }
-
-    // ------------------------- BELOW is modal code
-    createModal($claimid);
-    // ------------------------- ABOVE is modal code
+    createModal($claim_id);?> </label> <?php
 }
-        // end of while statement
-        ?> </label><input id="<?php echo $claimid; ?>" type="checkbox">
-            <ul> <span class="more">&hellip;</span>
 
-            <?php
-            // below is to continue recursion
-            $sql1 = "SELECT DISTINCT claimIDFlagger
-  from claimsdb, flagsdb
-        WHERE ? = claimIDFlagged AND flagType NOT LIKE 'Thesis Rival'"; // SQL with parameters
-            $stmt1 = $conn->prepare($sql1);
-            $stmt1->bind_param('i', $claimid);
-            $stmt1->execute();
-            $result1 = $stmt1->get_result(); // get the mysqli result
-            $numhits1 = mysqli_num_rows($result1);
-            // IF A CLAIM IS FLAGGED IT obtains flaggers that aren't rivals
-            // if its a thesis rival it will show up in the query above
-            // this is when the claim is the flagged. this is what gets pushed in the recursion.
+// starts two chains of recursion. one with normal root claims.
+// the other with root rivals. the rivals, of course, are put into the rival recursion.
+function sortclaims($claimID)
+{
+    $conn = db_connect();
+    $claim = get_claim($claimID, $conn);
+    if (!$claim) {
+        return;
+    }
+    // IF THIS CLAIM IS A FLAGGER this obtains the FLAGGER'S flagtype's and flagged.
+    // this is to find rival claims..this is literally JUST used for rivals.
+    // rivals have to be flaggers and flagged.
+    $flags = get_rival_flags($claimID, $conn);
+    $resultFlagType = $claimIDFlagger = $claimIDFlagged = '';
+    foreach ($flags as $f) {
+        $resultFlagType = $f['flagType'];
+        $claimIDFlagger = $f['claimIDFlagger'];
+        $claimIDFlagged = $f['claimIDFlagged'];
+    }
+    if ($resultFlagType == 'Thesis Rival') {
+        // echo ' <br> The flag ' . $claimIDFlagger . ' has a rival!: ' . '<br>';
+        // for THIS claimID - check for flaggers that aren't rival .. sort claim those
+        sortclaimsRival($claimIDFlagger);
+        // for the CORRESPONDING claimID - check for flaggers that aren't rival .. sort claim those.
+        sortclaimsRival($claimIDFlagged);
+        return;
+    }
+    ?>
+    <li><input id="<?php echo $claimID; ?>" type="checkbox">
+        <?php make_label_el($claimID, $claim, $resultFlagType); ?>
+        <ul><span class="more">• • •</span>
 
-            while ($user = $result1->fetch_assoc()) {
-                if (0 == $numhits1) {
-                } else {
-                    sortclaims($user['claimIDFlagger']);
-                }
-            }
-
-        // end while loop
-        // recursion finished here
-        ?></ul><?php
-    } // end of else statement
-} // end of function
+        <?php
+        // IF A CLAIM IS FLAGGED IT obtains flaggers that aren't rivals
+        // if its a thesis rival it will show up in the query above
+        // this is when the claim is the flagged. this is what gets pushed in the recursion.
+        // continue recursion
+        $result1 = get_non_flag_rivals($claimID, $conn); // get the mysqli result
+        foreach ($result1 as $user) {
+            sortclaims($user['claimIDFlagger']);
+        }?></ul><?php
+}
 
 /*
 This function has the same functionality as the sortClaims, but for rivals.
@@ -147,84 +174,41 @@ The key difference is handling the “mutualistic flagging” relationship that 
 It breaks an infinite loop that would otherwise occur if a rival was handled recursively in sortClaims().
 */
 
-function sortclaimsRIVAL($claimid)
+function sortclaimsRIVAL($claimID)
 {
     $conn = db_connect();
-
-    // BELOW IS SIMPLY FOR DISPLAY OF SUBJECT/TARGETP
-    // these aren't passed through the function so they must be obtained every interation
-    $dis = 'SELECT DISTINCT subject, targetP, active, supportMeans, reason, example
-  from claimsdb
-  where ? = claimID
-  ';
-    $st = $conn->prepare($dis);
-    $st->bind_param('i', $claimid);
-    $st->execute();
-    $disp = $st->get_result();
-    // ABOVE IS SIMPLY FOR DISPLAY
-
-    // BELOW IS JUST TO DISPLAY THE RIVAL PAIR
-    $sql1 = "SELECT DISTINCT claimIDFlagger
-  from claimsdb, flagsdb
-  where ? = claimIDFlagged AND flagType LIKE 'Thesis Rival'
-  ";
-    $stmt1 = $conn->prepare($sql1);
-    $stmt1->bind_param('i', $claimid);
-    $stmt1->execute();
-    $result1 = $stmt1->get_result();
-    $numhits1 = mysqli_num_rows($result1);
-    // above looks for normal non-rival flags for this rivaling claim.
-    while ($user = $result1->fetch_assoc()) {
+    // get the info for the claim being flagged
+    $claim = get_claim($claimID, $conn);
+    // look for normal non-rival flags for this rivaling claim.
+    $result1 = get_flagged_rivals($claimID, $conn);
+    foreach ($result1 as $user) {
         $rivaling = $user['claimIDFlagger'];
     }
-    // end while loop
-    // ABOVE IS JUST TO DISPLAY RIVAL PAIR
     ?>
 
-        <li> <label style="background:#FFFFE0" for="<?php echo $claimid; ?>"><?php while (
-    $d = $disp->fetch_assoc()
-) {
-    echo "<img width='100' height='20' src='assets/img/rivals.png'> <br><br>";
+        <li> <label style="background:#FFFFE0" for="<?php echo $claimID; ?>">
+        <?php
+        echo "<img width='100' height='20' src='assets/img/rivals.png'> <br><br>";
 
-    if (1 == $d['active']) {
-        // echo "<img width='50' height='50' src='assets/img/check_mark.php'> <br>! This claim is uncontested. <br>";
-    } else {
-        echo "<img src='assets/img/alert.png'> <br>";
-    }
+        if ($claim['active'] != 1) {
+            echo "<img src='assets/img/alert.png'> <br>";
+        }
 
-    echo '<h4>Contests #' . $rivaling . '</h4>';
-    echo '<h1>Thesis</h1>';
-    echo $d['subject'] . ' ' . $d['targetP'];
-    echo '<BR>' . $claimid;
-    // --------------------------- BELOW is modal code
-    createModal($claimid);
-    // --------------------------- ABOVE is modal code
-}// end of display fetching loop
-    ?>
+        echo '<h4>Contests #' . $rivaling . '</h4>';
+        echo '<h1>Thesis</h1>';
+        echo $claim['subject'] . ' ' . $claim['targetP'];
+        echo '<BR>' . $claimID;
+        createModal($claimID);
+        ?>
 
-        </label><input id="<?php echo $claimid; ?>" type="checkbox">
+        </label><input id="<?php echo $claimID; ?>" type="checkbox">
         <ul> <span class="more">&hellip;</span>
             <!--</font>-->
                 <?php
-                // below finds the flagger and continues the recursion by pushing it back to sortclaims recursion
-                $sql1 = "SELECT DISTINCT claimIDFlagger
-from claimsdb, flagsdb
-where ? = claimIDFlagged AND flagType NOT LIKE 'Thesis Rival'
-";
-                $stmt1 = $conn->prepare($sql1);
-                $stmt1->bind_param('i', $claimid);
-                $stmt1->execute();
-                $result1 = $stmt1->get_result();
-                $numhits1 = mysqli_num_rows($result1);
-                // above looks for normal non-rival flags for this rivaling claim.
+                $result1 = get_non_flag_rivals($claimID, $conn);
                 while ($user = $result1->fetch_assoc()) {
-                    if (0 == $numhits1) {
-                    } else {
-                        sortclaims($user['claimIDFlagger']);
-                    }
-                }// end while loop
-    // it's pushed - now the function is finished!
-    ?>
+                    sortclaims($user['claimIDFlagger']);
+                }?>
         </ul><?php
 } // end of rivalfunction
 ?>
