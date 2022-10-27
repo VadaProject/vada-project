da012a72ba1fa0196b643c4355bcac3f86c999b8
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-10-27 18:19:45 -0400

    flagging.php: Remove unused variables.

--- functions/flagging.php
+++ functions/flagging.php
@@ -10,11 +10,9 @@ use Database\Database;
 /**
  * This function is used in details.php. It contains the form UI for flagging an inference.
  */
-function flagging($claimIDFlaggedINSERT)
+function flagging($claimIDFlagged)
 {
-    $claimFlagged = Database::getClaim($claimIDFlaggedINSERT);
-    // $claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST = $ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID = $example = $URL = $rd = $reason = $flagType = $flagType = $flagTypeT = $flagTypeR = $flagTypeE = $flagURL = $flagSource = $flagID = $inferenceIDFlagger = $grammar = $active =
-    '';
+    $claimFlagged = Database::getClaim($claimIDFlagged);
     ?>
     <div class='scroll'> <?php  ?>
         <html>
@@ -24,18 +22,13 @@ function flagging($claimIDFlaggedINSERT)
             $topic = $topic;
             ?>
             <label>Topic (Read only)</label><br>
-            <input type="text" name="topic" value="<?php echo htmlspecialchars(
-                $topic
-            ); ?>" readonly><br>
+            <input type="text" name="topic"
+            value="<?php echo htmlspecialchars($topic); ?>" readonly><br>
         <div id="hideThesis">
             Enter your new claim.<br>
             <label><u>Subject</u> <u>Target Property</u></label><br>
-            <textarea class="subject" type="text" id="subject" name="subject" value="<?php echo htmlspecialchars(
-                $subject
-            ); ?>">Enter Subject</textarea>
-            <textarea class="targetP" type="text" id="targetP" name="targetP" value="<?php echo htmlspecialchars(
-                $targetP
-            ); ?>">Enter Target Property</textarea>
+            <textarea class="subject" type="text" id="subject" name="subject">Enter Subject</textarea>
+            <textarea class="targetP" type="text" id="targetP" name="targetP">Enter Target Property</textarea>
             <br>
             <u>
                 <p style="color:grey">Thesis Statement
@@ -66,8 +59,7 @@ function flagging($claimIDFlaggedINSERT)
                 <span id="explain-element"> Hint: Your reason statement should answer "Why?". You might think of the reason as what comes after 'because....'.</span>
             </div>
             <p style="color:black"><u>Reason</u><br>
-                <textarea class="reason" type="text" id="reason" name="reason"
-                value="<?php echo htmlspecialchars($reason); ?>">
+                <textarea class="reason" type="text" id="reason" name="reason">
                     Enter Reason Property
                 </textarea>
             </p>
@@ -94,9 +86,7 @@ function flagging($claimIDFlaggedINSERT)
             } ?>
             as in the case of:
             <br>
-            <textarea id="example" name="example" value="<?php echo htmlspecialchars(
-                $example
-            ); ?>">Enter Example</textarea>
+            <textarea id="example" name="example">Enter Example</textarea>
         </div>
         <div id="perceptionHint">
             <div id="some-div">
@@ -111,47 +101,29 @@ function flagging($claimIDFlaggedINSERT)
                 <span id="explain-element"> Hint: The transcription MUST be a quotation from the source with no additional dialogue.</span>
             </div>
         </div>
-        <textarea id="transcription" name="transcription" value="<?php echo htmlspecialchars(
-            $transcription
-        ); ?>">Transcription</textarea><br>
+        <textarea id="transcription" name="transcription">Transcription</textarea><br>
         <div id="hiddenCitation">
             <u>Citation</u>
             <?php $author = $title = $publication = $date = ''; ?>
             <div id="some-div">
                 <img src="assets/img/question_mark.png">
                 <span id="explain-element"> Please include as applicable: author, title, publication, and date of publication.</span>
-                <br><textarea id="author" name="author" value="<?php echo htmlspecialchars(
-                    $author
-                ); ?>">Author</textarea><br>
-                <textarea id="title" name="title" value="<?php echo htmlspecialchars(
-                    $title
-                ); ?>">Title</textarea><br>
-                <textarea id="publication" name="publication" value="<?php echo htmlspecialchars(
-                    $publication
-                ); ?>">Publication</textarea><br>
-                <textarea id="date" name="date" value="<?php echo htmlspecialchars(
-                    $date
-                ); ?>">Date of Publication</textarea><br>
-                <textarea id="citationURL" name="citationURL" value="<?php echo htmlspecialchars(
-                    $citationURL
-                ); ?>">URL of Citation</textarea><br>
+                <br><textarea id="author" name="author">Author</textarea><br>
+                <textarea id="title" name="title">Title</textarea><br>
+                <textarea id="publication" name="publication">Publication</textarea><br>
+                <textarea id="date" name="date">Date of Publication</textarea><br>
+                <textarea id="citationURL" name="citationURL">URL of Citation</textarea><br>
             </div>
         </div>
-        <textarea id="citation" name="citation" hidden="hidden" value="<?php echo htmlspecialchars(
-            $citation
-        ); ?>">Empty Citation</textarea><br>
+        <textarea id="citation" name="citation" hidden="hidden">Empty Citation</textarea><br>
         <div id="hiddenURL">
             <u>URL</u>
         </div>
-        <textarea id="url" name="url" value="<?php echo htmlspecialchars(
-            $url
-        ); ?>">Enter URL</textarea><br>
+        <textarea id="url" name="url">Enter URL</textarea><br>
         <div id="hiddenTS">
             <u>Timestamp of content</u>
         </div>
-        <textarea id="vidtimestamp" name="vidtimestamp" value="<?php echo htmlspecialchars(
-            $vidtimestamp
-        ); ?>">Enter timestamp of specified material</textarea>
+        <textarea id="vidtimestamp" name="vidtimestamp">Enter timestamp of specified material</textarea>
         <script>
             // This is for reason
             var $jsReason = document.querySelector('.reason');
