VadaProject/vada-project

commit 938c4dfe718f7ed72416fe66f3aacbce120578e9

Refactor db_connect.php
Change the file into a function that returns the $conn value, rather
than assigning it.

details.php
@@ -1,4 +1,4 @@
-<?php include 'config/db_connect.php'; ?>
+<?php require_once 'config/db_connect.php'; ?>
 <?php include 'includes/page_top.php'; ?>
 <main class="page-container text-center">
 <?php

about.php
@@ -1,4 +1,3 @@
-<?php include 'config/db_connect.php'; ?>
 <?php include 'includes/page_top.php'; ?>
 <main class="page-container">
     <h2>About</h2>

add.php
@@ -1,10 +1,7 @@
-<?php include 'config/db_connect.php'; ?>
 <?php include 'includes/page_top.php'; ?>
 <?php
-$claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST =
-$ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID =
-$example = $URL = $rd = $reason = $flagType = $flagType = $flagTypeT = $flagTypeR = $flagTypeE =
-$flagURL = $flagSource = $flagID = $inferenceIDFlagger = $grammar = $active = '';
+$claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST = $ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID = $example = $URL = $rd = $reason = $flagType = $flagType = $flagTypeT = $flagTypeR = $flagTypeE = $flagURL = $flagSource = $flagID = $inferenceIDFlagger = $grammar = $active =
+    '';
 session_start();
 ?>
 <main class="page-container text-center">
@@ -35,18 +32,26 @@ session_start();
     <form method="POST" id="myForm" action="insert.php">
         <?php if (isset($_GET['topic'])) { ?>
         <label>Topic</label><br>
-        <input type="text" name="topic" value="<?php echo $_GET['topic']; ?>" readonly><br>
+        <input type="text" name="topic" value="<?php echo $_GET[
+            'topic'
+        ]; ?>" readonly><br>
         <?php } else { ?>
         <label>Topic</label><br>
-        <input type="text" name="topic" value="<?php echo htmlspecialchars($topic); ?>"><br>
+        <input type="text" name="topic" value="<?php echo htmlspecialchars(
+            $topic
+        ); ?>"><br>
         <?php } ?>
         <p style="color:#000000" ;>
             <label><u>Subject</u>
                 <u>Target Property </u></label><br>
             <textarea class="subject" type="text" id="subject" name="subject"
-                value="<?php echo htmlspecialchars($subject); ?>">Enter Subject</textarea>
+                value="<?php echo htmlspecialchars(
+                    $subject
+                ); ?>">Enter Subject</textarea>
             <textarea class="targetP" type="text" id="targetP" name="targetP"
-                value="<?php echo htmlspecialchars($targetP); ?>">Enter Target Property</textarea>
+                value="<?php echo htmlspecialchars(
+                    $targetP
+                ); ?>">Enter Target Property</textarea>
             <br>
             <u>
                 <p style="color:grey">Thesis Statement
@@ -79,7 +84,9 @@ session_start();
             </div>
             <p style="color:black"><u>Reason</u><br>
                 <textarea class="reason" type="text" id="reason" name="reason"
-                    value="<?php echo htmlspecialchars($reason); ?>">Enter Reason Property</textarea>
+                    value="<?php echo htmlspecialchars(
+                        $reason
+                    ); ?>">Enter Reason Property</textarea>
             </p>
             <u> Reason Statement </u><br>
             <span class="jsValue5">subject</span> <span class="jsValue6">reason</span>
@@ -92,14 +99,24 @@ session_start();
             as in the case of:
         </div>
         <br>
-        <textarea id="example" name="example" value="<?php echo htmlspecialchars($example); ?>">Enter Example</textarea>
+        <textarea id="example" name="example" value="<?php echo htmlspecialchars(
+            $example
+        ); ?>">Enter Example</textarea>
         <textarea id="transcription" name="transcription"
-            value="<?php echo htmlspecialchars($transcription); ?>">Transcription </textarea><br>
-        <textarea id="citation" name="citation" value="<?php echo htmlspecialchars($citation); ?>">
+            value="<?php echo htmlspecialchars(
+                $transcription
+            ); ?>">Transcription </textarea><br>
+        <textarea id="citation" name="citation" value="<?php echo htmlspecialchars(
+            $citation
+        ); ?>">
             Please include: Author, title, publication, and date of publication.</textarea><br>
-        <textarea id="url" name="url" value="<?php echo htmlspecialchars($url); ?>">Enter URL</textarea><br>
+        <textarea id="url" name="url" value="<?php echo htmlspecialchars(
+            $url
+        ); ?>">Enter URL</textarea><br>
         <textarea id="vidtimestamp" name="vidtimestamp"
-            value="<?php echo htmlspecialchars($vidtimestamp); ?>">Enter timestamp of specified material</textarea>
+            value="<?php echo htmlspecialchars(
+                $vidtimestamp
+            ); ?>">Enter timestamp of specified material</textarea>
         <script>
         // This is for reason
         var $jsReason = document.querySelector('.reason');

adnanindex.php
@@ -1,9 +1,10 @@
 <?php
+require_once 'config/db_connect.php';
 // Check if user has requested to get detail
 if (isset($_POST['get_data'])) {
     // Get the ID of customer user has selected
     $id = $_POST['id'];
-    include 'config/db_connect.php';
+    $conn = db_connect();
     // Getting specific customer's detail
     $sql = "SELECT * FROM claimsdb WHERE claimID='{$id}'";
     $result = mysqli_query($conn, $sql);
@@ -18,7 +19,7 @@ if (isset($_POST['get_data'])) {
 
 <?php
 // Connecting with database and executing query
-include 'config/db_connect.php';
+$conn = db_connect();
 $sql = 'SELECT * FROM claimsdb';
 $result = mysqli_query($conn, $sql);
 ?>

ajaxindex.php
@@ -1,5 +1,6 @@
-<?php include 'includes/page_top.php'; ?>
-<?php require 'config/db_connect.php';
+<?php require_once 'config/db_connect.php'; ?>
+<?php require 'includes/page_top.php'; ?>
+<?php
 require 'functions/sortClaims.php';
 require 'functions/doesThesisFlag.php';
 require 'functions/noSupports.php';
@@ -7,6 +8,8 @@ require 'functions/restoreActivity.php';
 require 'functions/createModal.php';
 require 'functions/haveRival.php';
 
+
+$conn = db_connect();
 if (isset($_GET['topic'])) {
     $topic = mysqli_real_escape_string($conn, $_GET['topic']);
 } // end isset check?>

config/db_connect.php
@@ -1,9 +1,14 @@
 <?php
 
-// connect to database
-$conn = mysqli_connect('localhost', 'amydonahue', 'amypassword', 'vadaProject');
+function db_connect()
+{
+    // connect to database
+    $conn = mysqli_connect('localhost', 'amydonahue', 'amypassword', 'vadaProject');
 
-// check connection
-if (!$conn) {
-    echo 'Connection Error: ' . mysqli_connect_error();
+    // check connection
+    if (!$conn) {
+        echo 'Connection Error: ' . mysqli_connect_error();
+        return false;
+    }
+    return $conn;
 }

functions/createModal.php
@@ -2,14 +2,15 @@
 
 function createModal($claimid)
 {
-    require __DIR__ . '/../config/db_connect.php';
+    require_once __DIR__ . '/../config/db_connect.php';
+    $conn = db_connect();
 
     // Check if user has requested to get detail
     if (isset($_POST['get_data'])) {
         // Get the ID of customer user has selected
         $id = $_POST['id'];
 
-        require __DIR__ . '/../config/db_connect.php';
+        require_once __DIR__ . '/../config/db_connect.php';
 
         // Getting specific customer's detail
         $sql = "SELECT * FROM claimsdb WHERE claimID='{$id}'";
@@ -24,7 +25,6 @@ function createModal($claimid)
     }
 
     // Connecting with database and executing query
-    include 'config/db_connect.php';
     $sql = "SELECT * FROM claimsdb WHERE claimID = '{$claimid}'";
     $result = mysqli_query($conn, $sql);
     ?>

functions/doesThesisFlag.php
@@ -1,7 +1,8 @@
 <?php
 function doesThesisFlag($claimid)
     {
-    require __DIR__ . '/../config/db_connect.php';
+    require_once __DIR__ . '/../config/db_connect.php';
+    $conn = db_connect();
     $answer = 'false';
     $act2 = "SELECT DISTINCT claimIDFlagger
     from flagsdb
@@ -31,7 +32,8 @@ function doesThesisFlag($claimid)
                 }
 function doesThesisFlagRival($claimid)
 {
-                    include 'config/db_connect.php';
+                    require_once 'config/db_connect.php';
+                    $conn = db_connect();
                     $answer = 'false';
 
                     $act2 = "SELECT DISTINCT claimIDFlagger

commit 1d05a27d0e19548e3138512dde631761e8a06e0e

Fix crash in details.php.
Merged commit of the following:

commit 969da65a0835214a021aee6deb45ffccc469cebe
Author: Mae Morella <4561733+morellam-dev@users.noreply.github.com>
Date:   Thu Oct 27 15:19:09 2022 -0400

    Remove unused variables in details.php

commit 86ca034d0a6ccc2f129bda92129b3e1a8bf8b9b5
Author: Mae Morella <4561733+morellam-dev@users.noreply.github.com>
Date:   Thu Oct 27 15:07:22 2022 -0400

    Add missing db_connect call to details.php

    Fixed a regression created in commit
    938c4dfe718f7ed72416fe66f3aacbce120578e9 which made the page crash.

details.php
@@ -1,4 +1,4 @@
-<?php require_once 'config/db_connect.php'; ?>
+<?php  ?>
 <?php include 'includes/page_top.php'; ?>
 <main class="page-container text-center">
 <?php
@@ -6,9 +6,12 @@
 /*
 This displays the argument in full detail and pushes any user interaction/submissions to add.php.
 */
+require_once 'config/db_connect.php';
+require_once 'functions/flagging.php';
+require_once 'functions/retrieveFunctions.php';
 
-require 'functions/flagging.php';
-require 'functions/retrieveFunctions.php';
+$conn = db_connect();
+
 $claimID = $temp = $result = $topic = $array = $claim_fk = $IclaimID = $thesisST = $reasonST = $ruleST = $NewOld = $oldClaim = $subject = $targetP = $supportMeans = $supportforID = $supportID = $example = $URL = $rd = $reason = $flagType = $flagTypeT = $flagTypeR = $flagTypeE = $flagURL = $flagSource = $flagID = $inferenceIDFlagger = $active = $dSubject = $dTargetP = $domain = $supportingDropCheck =
     '';
 if (isset($_GET['id'])) {
@@ -430,15 +433,6 @@ $_POST['FOS'] = 'flagging';
         $("#submit").click(function() {
             window.alert("Submitted!");
             window.location.assign("ajaxindex.php?topic=<?php echo $topic; ?>");
-            /*
-  if (union.options[union.selectedIndex].value === 'Tarka') {
-window.alert("testing to redirect tarka!");
-$newid = $claimID++;
-window.location.assign("details.php?id=<?php echo $newid; ?>");
-  }
-  else
-  { window.location.assign("ajaxindex.php?topic=<?php echo $topic; ?>"); }
-  */
             $.post($("#myForm").attr("action"),
                 $("#myForm :input").serializeArray(),
                 function(info) {