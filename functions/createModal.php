987faff63d2268c16b9cd7257baac0ff14bd45b5
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-10-28 18:49:30 -0400

    createModal: refactor, remove unused AJAX response

--- functions/createModal.php
+++ functions/createModal.php
@@ -1,43 +1,21 @@
 <?php
 
-function createModal($claimid)
+
+// TODO: refactor this back into the other claim rendering logic
+// The function name is wrong; it doesn't create a modal.
+
+/**
+ * This function echoes the HTML element for the Details button
+ * used to show the modal.
+ */
+function createModal($claimID)
 {
-    require_once __DIR__ . '/../config/db_connect.php';
-    $conn = db_connect();
-
-    // Check if user has requested to get detail
-    if (isset($_POST['get_data'])) {
-        // Get the ID of customer user has selected
-        $id = $_POST['id'];
-
-        require_once __DIR__ . '/../config/db_connect.php';
-
-        // Getting specific customer's detail
-        $sql = "SELECT * FROM claimsdb WHERE claimID='{$id}'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_object($result);
-
-        // Important to echo the record in JSON format
-        echo json_encode($row);
-
-        // Important to stop further executing the script on AJAX by following line
-        exit();
-    }
-
-    // Connecting with database and executing query
-    $sql = "SELECT * FROM claimsdb WHERE claimID = '{$claimid}'";
-    $result = mysqli_query($conn, $sql);
-    ?>
-
-            <!-- Creating table heading -->
-            <div class="container">
-
-                <!-- Display dynamic records from database -->
-            <?php while ($row = mysqli_fetch_object($result)) { ?>
-                <button class="btn btn-primary" onclick="loadData(this.getAttribute('data-id'));"
-                    data-id="<?php echo $row->claimID; ?>">
-                    Details
-                </button>
-                </div>
-            <?php }
+?>
+    <div class="container">
+        <button class="btn btn-primary" onclick="loadData(this.getAttribute('data-id'));"
+        data-id="<?php echo $claimID; ?>">
+        Details
+        </button>
+    </div>
+    <?php
 }
