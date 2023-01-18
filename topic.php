9d9425bb3ffd2fcd05b0e4148868885ba2335bf0
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2023-01-18 16:35:52 -0500

    Update claims UI + site theme
    - Improve Button style
    - Simplify claim tree rendering
    - Refactor CSS theme
    - Replace low-res Vada Project logo with SVG
    - Header: fix weird formatting on a.active link
    - Make text input elements bigger and more padded

--- topic.php
+++ topic.php
@@ -16,18 +16,11 @@ if (isset($_GET['topic'])) {
 ?>
 
 <div class="wrapper">
+    <h2>Topic: <?php echo $topic; ?></h2>
+    <p>
+        <a class="btn btn-primary" href="add.php?topic=<?php echo $topic; ?>">Add New Claim</a>
+    </p>
     <ul>
-        <li class="noline">
-            <span>
-                <p><a href="add.php?topic=<?php echo $topic; ?>">Add New Claim To
-                        This Topic</a></p>
-            </span>
-            <h3>TOPIC: <?php echo $topic; ?></h3>
-
-
-            </center>
-            <center>
-
             <?php
             $root_claim = Database::getAllRootClaimIDs($topic);
             foreach ($root_claim as $claim_id) {
@@ -48,23 +41,7 @@ if (isset($_GET['topic'])) {
                 sortclaimsRIVAL($claim_id);
             }
             ?>
-            <!-- Modal -->
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
-
-                <div class="modal-dialog">
-                    <div class="modal-content-a">
-                        <b>Claim Details</b> <br>
-                        <div class="modal-content-b">
-
-                            <div class="modal-header">
-                                <h4 class="modal-title"></h4>
-                            </div>
-                            <div id="modal-body">Press ESC button to exit.</div>
-                        </div> <!-- modal-content-b -->
-                    </div><!-- /.modal-content-a-->
-                </div><!-- /.modal-dialog -->
-
-            </div>
+    </ul>
 </div>
 <?php include 'includes/page_bottom.php'; ?>
 <?php mysqli_close($conn); ?>
