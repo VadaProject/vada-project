fd96dadc00190ecca085d756d025326f6689748f
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2023-02-09 00:36:41 -0500

    Only layer topbar and footer on topic.php
    - Increased modal z-index to prevent fighting

--- topic.php
+++ topic.php
@@ -7,6 +7,7 @@ require_once 'functions/noSupports.php';
 require_once 'functions/restoreActivity.php';
 require_once 'functions/Database.php';
 use Database\Database;
+
 $conn = db_connect();
 if (isset($_GET['topic'])) {
     $topic = mysqli_real_escape_string($conn, $_GET['topic']);
@@ -14,33 +15,42 @@ if (isset($_GET['topic'])) {
 
 // end isset check
 ?>
+<style>
+    footer,
+    .topnav {
+        position: fixed;
+    }
+</style>
 
 <div class="wrapper">
-    <h2>Topic: <?php echo $topic; ?></h2>
+    <h2>Topic:
+        <?php echo $topic; ?>
+    </h2>
     <p>
-        <a class="btn btn-primary" href="add.php?topic=<?php echo $topic; ?>">Add New Claim</a>
+        <a class="btn btn-primary"
+            href="add.php?topic=<?php echo $topic; ?>">Add New Claim</a>
     </p>
     <ul>
-            <?php
-            $root_claim = Database::getAllRootClaimIDs($topic);
-            foreach ($root_claim as $claim_id) {
-                restoreActivity($claim_id);
-            }
-            foreach ($root_claim as $claim_id) {
-                sortclaims($claim_id);
-            }
-            $root_rivals = Database::getRootRivals($topic);
-            foreach ($root_rivals as $claim_id) {
-                restoreActivityRIVAL($claim_id);
-            }
-            $thesis_rivals = Database::getAllThesisRivals($topic);
-            foreach ($thesis_rivals as $claim_id) {
-                restoreActivityRIVAL($claim_id);
-            }
-            foreach ($root_rivals as $claim_id) {
-                sortclaimsRIVAL($claim_id);
-            }
-            ?>
+        <?php
+        $root_claim = Database::getAllRootClaimIDs($topic);
+        foreach ($root_claim as $claim_id) {
+            restoreActivity($claim_id);
+        }
+        foreach ($root_claim as $claim_id) {
+            sortclaims($claim_id);
+        }
+        $root_rivals = Database::getRootRivals($topic);
+        foreach ($root_rivals as $claim_id) {
+            restoreActivityRIVAL($claim_id);
+        }
+        $thesis_rivals = Database::getAllThesisRivals($topic);
+        foreach ($thesis_rivals as $claim_id) {
+            restoreActivityRIVAL($claim_id);
+        }
+        foreach ($root_rivals as $claim_id) {
+            sortclaimsRIVAL($claim_id);
+        }
+        ?>
     </ul>
 </div>
 <?php include 'includes/page_bottom.php'; ?>
