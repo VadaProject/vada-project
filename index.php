3e7c442a1cb8dca3a66f23a973e33cbf0e6497ab
amydonahue <63516093+amydonahue@users.noreply.github.com>
2023-04-25 13:17:53 -0400

    Change "Indian logic" to "Sanskritic logic"

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require 'includes/page_top.php'; ?>
 <main class="page-container text-center">
     <h2>Welcome!</h2>
-    <p>This is a website for discussion and learning that is based on Indian traditions of epistemology, logic and debate. Users can create new claims using protocols of <i>vāda</i> and <i>pramāṇavāda</i>, or participate in and observe pre-existing debates.</p>
+    <p>This is a website for discussion and learning that is based on Sanskritic traditions of epistemology, logic and debate. Users can create new claims using protocols of <i>vāda</i> and <i>pramāṇavāda</i>, or participate in and observe pre-existing debates.</p>
     <h3>Topics to select from:</h3>
     <?php
     $conn = db_connect();
